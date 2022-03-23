75917c54a1cb44b4627a75fa8a3229436421de60
Kendra Birringer <57136158+kendrabirringer@users.noreply.github.com>
2022-03-23 15:33:08 +0100

    Create React App

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+function App() {
+    return (
+    <div>
+        <Header />
+        <Sidebar />
+
+
+    </div>
+    );
+}
+
+export default App;
