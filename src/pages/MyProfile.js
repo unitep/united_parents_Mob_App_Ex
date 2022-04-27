6ada88349bab34a365074ba1cb56982d5354fe67
Kendra Birringer <57136158+kendrabirringer@users.noreply.github.com>
2022-04-27 09:58:10 +0200

    Add some style to My Profile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -3,7 +3,32 @@ import React from "react";
 function MyProfile() {
     return (
         <div>
-            <h1>Mein Profil</h1>
+            {/* TO DO:
+                * get all the specific user info from the DB
+              */}
+            <div className="content-box profile">
+                <div className="profile-img">
+                    <img className="circle-img" src="https://i.pravatar.cc/200"></img>
+                </div>
+                <div className="profile-info">
+                    <h1>Vorname Nachname</h1>
+                    <h2><strong>Alter: </strong>22</h2>
+                    <h2><strong>Hochschule: </strong>Frankfurt University of Applied Sciences</h2>
+                    <h2><strong>Studiengang: </strong>International Business</h2>
+                    <h2><strong>Kind(er): </strong>1</h2>
+                    <h2><strong>Alter: </strong>2</h2>
+                </div>
+            </div>
+
+            <div className="content-box about-me">
+                <h1>Über mich:</h1>
+                <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                    ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                    parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+                    pellentesque eu, pretium.</h2>
+            </div>
+
+            {/* TO DO: implement week calendar */}
         </div>
     );
 }
--- src/pages/SearchUser.js
+++ src/pages/SearchUser.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 /**
- * TODO: get input PLZ, serach it in firebase and present result in a list
+ * TO DO: 
+ * get input PLZ, search it in the DB and present result
  * implement API for the radius
  *
  */
@@ -44,17 +45,17 @@ function SearchUser() {
                 </form>
 
                 {/*
-                TODO:
-                Move the result list in onSubmit function,
+                TO DO:
+                * Move the result list in onSubmit function,
                 so that it only shows after clicking on the serach button.
-                Handle output if there are no results!
-                Also handle onClick:
+                * Handle output if there are no results!
+                * Also handle onClick:
                     * profile image: link to user profile
                     * send message button: link to user chat
                  */}
 
                 {/* Show this only, if there are no results */}
-                <div className="no-results">
+                <div className="content-box no-results">
                     <h1>Keinen User gefunden!</h1>
                     <p>Ändere den Suchradius oder gib eine andere PLZ ein.</p>
                 </div>
@@ -66,7 +67,7 @@ function SearchUser() {
                             {/* Catch corresponding user profile image from DB
                             and link user to the corresponding user profile on click on the image
                          */}
-                            <img className="circle-img" src="https://i.pravatar.cc/120"></img>
+                            <img className="circle-img" src="https://i.pravatar.cc/200"></img>
                             {/* Catch corresponding user full name from DB  */}
                             <h1>Vorname Nachname</h1>
                             {/* Catch corresponding user ZIP code from DB  */}
@@ -77,10 +78,10 @@ function SearchUser() {
                             </button>
                         </div>
                     </div>
-
+                    
                     <div className="column">
                         <div className="zip-result-card">
-                            <img className="circle-img" src="https://i.pravatar.cc/120"></img>
+                            <img className="circle-img" src="https://i.pravatar.cc/200"></img>
                             <h1>Vorname Nachname</h1>
                             <p>Postleitzahl Stadt</p>
                             <button className="send-msg-btn">
@@ -91,7 +92,7 @@ function SearchUser() {
 
                     <div class="column">
                         <div className="zip-result-card">
-                            <img className="circle-img" src="https://i.pravatar.cc/120"></img>
+                            <img className="circle-img" src="https://i.pravatar.cc/200"></img>
                             <h1>Vorname Nachname</h1>
                             <p>Postleitzahl Stadt</p>
                             <button className="send-msg-btn">
@@ -102,7 +103,18 @@ function SearchUser() {
 
                     <div class="column">
                         <div className="zip-result-card">
-                            <img className="circle-img" src="https://i.pravatar.cc/120"></img>
+                            <img className="circle-img" src="https://i.pravatar.cc/200"></img>
+                            <h1>Vorname Nachname</h1>
+                            <p>Postleitzahl Stadt</p>
+                            <button className="send-msg-btn">
+                                <span className="material-icons">forum</span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div className="zip-result-card">
+                            <img className="circle-img" src="https://i.pravatar.cc/200"></img>
                             <h1>Vorname Nachname</h1>
                             <p>Postleitzahl Stadt</p>
                             <button className="send-msg-btn">
